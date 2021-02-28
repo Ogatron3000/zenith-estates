c9d978f063aa5876fa6987213b25716e9db26b7b
Ivan Mijanovic <ivanmijanovic18@gmail.com>
2021-02-28 21:01:36 +0100

    fixed details page edit && delete links

--- app/views/real_estates/show.php
+++ app/views/real_estates/show.php
@@ -29,9 +29,9 @@ partial('header'); ?>
 
             <div class="flex justify-between mt-4">
                 <button class="w-1/2 py-2 mr-4 bg-blue-400 hover:bg-blue-300 rounded-full text-white">
-                    <a href=<?= "real-estates/{$real_estate->id}/edit" ?>>Edit</a>
+                    <a href=<?= "{$real_estate->id}/edit" ?>>Edit</a>
                 </button>
-                <form action="<?= "real-estates/{$real_estate->id}" ?>"
+                <form action="<?= "{$real_estate->id}" ?>"
                       method="POST"
                       class="w-1/2">
                     <input type="hidden" name="method" value="DELETE">
