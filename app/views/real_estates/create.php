d93bc17865339fd7f69c0a1e74d5549fb37f2797
Ivan Mijanovic <ivanmijanovic18@gmail.com>
2021-02-28 19:37:14 +0100

    html && css

--- app/views/real_estates/create.php
+++ app/views/real_estates/create.php
@@ -2,7 +2,7 @@
 partial('header'); ?>
 
     <div class="w-5/12 mx-auto my-12 p-12 text-white bg-indigo-300">
-        <h1 class="mb-8 text-2xl text-center">New Real Estate Ad Form</h1>
+        <h1 class="mb-8 text-2xl text-center">Add Real Estate Ad</h1>
         <form action="../real-estates"
               method="POST"
               enctype="multipart/form-data">
@@ -12,20 +12,15 @@ partial('header'); ?>
                 <select name="city_id"
                         id="city"
                         class="p-1 mt-2 text-black">
-                    <?php
-                    foreach ($cities as $key => $value): ?>
+                    <?php foreach ($cities as $key => $value): ?>
                         <option value="<?= $cities[$key]->id ?>"
-                            <?= isset($old) && $old['city_id'] === $cities[$key]->id
-                                ? 'selected' : '' ?>
+                            <?= isset($old) && $old['city_id'] === $cities[$key]->id ? 'selected' : '' ?>
                         >
                             <?= $cities[$key]->name ?>
                         </option>
-                    <?php
-                    endforeach; ?>
+                    <?php endforeach; ?>
                 </select>
-                <?= isset($errors) && array_key_exists('city_id', $errors)
-                    ? '<p class="text-sm text-red-500">' . $errors['city_id']
-                    . '</p>' : '' ?>
+                <?= isset($errors) && array_key_exists('city_id', $errors) ? '<p class="mt-1 text-sm text-red-500">' . $errors['city_id'] . '</p>' : '' ?>
             </div>
 
             <div class="flex flex-col mb-4">
@@ -33,20 +28,15 @@ partial('header'); ?>
                 <select name="ad_type_id"
                         id="ad_type"
                         class="p-1 mt-2 text-black">
-                    <?php
-                    foreach ($ad_types as $key => $value): ?>
+                    <?php foreach ($ad_types as $key => $value): ?>
                         <option value="<?= $ad_types[$key]->id ?>"
-                            <?= isset($old) && $old['ad_type_id'] === $ad_types[$key]->id
-                                ? 'selected' : '' ?>
+                            <?= isset($old) && $old['ad_type_id'] === $ad_types[$key]->id ? 'selected' : '' ?>
                         >
                             <?= $ad_types[$key]->name ?>
                         </option>
-                    <?php
-                    endforeach; ?>
+                    <?php endforeach; ?>
                 </select>
-                <?= isset($errors) && array_key_exists('ad_type_id', $errors)
-                    ? '<p class="text-sm text-red-500">' . $errors['ad_type_id']
-                    . '</p>' : '' ?>
+                <?= isset($errors) && array_key_exists('ad_type_id', $errors) ? '<p class="mt-1 text-sm text-red-500">' . $errors['ad_type_id'] . '</p>' : '' ?>
             </div>
 
             <div class="flex flex-col mb-4">
@@ -54,20 +44,15 @@ partial('header'); ?>
                 <select name="re_type_id"
                         id="re_type"
                         class="p-1 mt-2 text-black">
-                    <?php
-                    foreach ($re_types as $key => $value): ?>
+                    <?php foreach ($re_types as $key => $value): ?>
                         <option value="<?= $re_types[$key]->id ?>"
-                            <?= isset($old) && $old['re_type_id'] === $re_types[$key]->id
-                                ? 'selected' : '' ?>
+                            <?= isset($old) && $old['re_type_id'] === $re_types[$key]->id ? 'selected' : '' ?>
                         >
                             <?= $re_types[$key]->name ?>
                         </option>
-                    <?php
-                    endforeach; ?>
+                    <?php endforeach; ?>
                 </select>
-                <?= isset($errors) && array_key_exists('re_type_id', $errors)
-                    ? '<p class="text-sm text-red-500">' . $errors['re_type_id']
-                    . '</p>' : '' ?>
+                <?= isset($errors) && array_key_exists('re_type_id', $errors) ? '<p class="mt-1 text-sm text-red-500">' . $errors['re_type_id'] . '</p>' : '' ?>
             </div>
 
             <div class="flex flex-col mb-4">
@@ -77,9 +62,7 @@ partial('header'); ?>
                        id="area"
                        value="<?= isset($old) ? $old['area'] : '' ?>"
                        class="p-1 mt-2 text-black">
-                <?= isset($errors) && array_key_exists('area', $errors)
-                    ? '<p class="text-sm text-red-500">' . $errors['area']
-                    . '</p>' : '' ?>
+                <?= isset($errors) && array_key_exists('area', $errors) ? '<p class="mt-1 text-sm text-red-500">' . $errors['area'] . '</p>' : '' ?>
             </div>
 
             <div class="flex flex-col mb-4">
@@ -89,9 +72,7 @@ partial('header'); ?>
                        id="price"
                        value="<?= isset($old) ? $old['price'] : '' ?>"
                        class="p-1 mt-2 text-black">
-                <?= isset($errors) && array_key_exists('price', $errors)
-                    ? '<p class="text-sm text-red-500">' . $errors['price']
-                    . '</p>' : '' ?>
+                <?= isset($errors) && array_key_exists('price', $errors) ? '<p class="mt-1 text-sm text-red-500">' . $errors['price'] . '</p>' : '' ?>
             </div>
 
             <div class="flex flex-col mb-4">
@@ -101,9 +82,7 @@ partial('header'); ?>
                        id="year"
                        value="<?= isset($old) ? $old['year'] : '' ?>"
                        class="p-1 mt-2 text-black">
-                <?= isset($errors) && array_key_exists('year', $errors)
-                    ? '<p class="text-sm text-red-500">' . $errors['year']
-                    . '</p>' : '' ?>
+                <?= isset($errors) && array_key_exists('year', $errors) ? '<p class="mt-1 text-sm text-red-500">' . $errors['year'] . '</p>' : '' ?>
             </div>
 
             <div class="flex flex-col mb-4">
@@ -111,9 +90,7 @@ partial('header'); ?>
                 <textarea name="description"
                           id="description"
                           class="p-1 mt-2 text-black"><?= isset($old) ? $old['description'] : '' ?></textarea>
-                <?= isset($errors) && array_key_exists('description', $errors)
-                    ? '<p class="text-sm text-red-500">'
-                    . $errors['description'] . '</p>' : '' ?>
+                <?= isset($errors) && array_key_exists('description', $errors) ? '<p class="mt-1 text-sm text-red-500">' . $errors['description'] . '</p>' : '' ?>
             </div>
 
 
@@ -124,9 +101,7 @@ partial('header'); ?>
                        id="photos"
                        multiple
                        class="p-2 mt-2 border-2 border-white">
-                <?= isset($errors) && array_key_exists('photos', $errors)
-                    ? '<p class="text-sm text-red-500">' . $errors['photos']
-                    . '</p>' : '' ?>
+                <?= isset($errors) && array_key_exists('photos', $errors) ? '<p class="text-sm text-red-500">' . $errors['photos'] . '</p>' : '' ?>
             </div>
 
             <button class="p-3 mt-12 bg-indigo-500 w-full text-white text-lg">Submit</button>
--- app/views/real_estates/edit.php
+++ app/views/real_estates/edit.php
@@ -1,30 +1,34 @@
 <?php
 partial('header'); ?>
 
-    <form action="./" method="POST" enctype="multipart/form-data" class="p-6 bg-yellow-200">
+    <div class="w-5/12 mx-auto my-12 p-12 text-white bg-indigo-300">
+    <h1 class="mb-8 text-2xl text-center">Edit Real Estate Ad</h1>
+    <form action="./" method="POST" enctype="multipart/form-data">
 
         <input type="hidden" name="method" value="PUT">
         <input type="hidden" name="id" value="<?= $real_estate->id ?>">
 
-        <div class="flex flex-col w-60 mb-4">
+        <div class="flex flex-col mb-4">
             <label for="city">City</label>
             <select name="city_id"
                     id="re_type"
-                    class="p-1 border border-black">
+                    class="p-1 mt-2 text-black">
                 <?php foreach ($cities as $key => $value): ?>
                     <option value="<?= $cities[$key]->id ?>"
-                        <?= $real_estate->city($real_estate->city_id) === $cities[$key]->name ? 'selected' : '' ?>>
+                        <?= $real_estate->city($real_estate->city_id) === $cities[$key]->name ? 'selected' : '' ?>
+                    >
                         <?= $cities[$key]->name ?>
                     </option>
                 <?php endforeach; ?>
             </select>
+            <?= isset($errors) && array_key_exists('city_id', $errors) ? '<p class="mt-1 text-sm text-red-500">' . $errors['city_id'] . '</p>' : '' ?>
         </div>
 
-        <div class="flex flex-col w-60 mb-4">
+        <div class="flex flex-col mb-4">
             <label for="city">Ad Type</label>
             <select name="ad_type_id"
                     id="re_type"
-                    class="p-1 border border-black">
+                    class="p-1 mt-2 text-black">
                 <?php foreach ($ad_types as $key => $value): ?>
                     <option value="<?= $ad_types[$key]->id ?>"
                         <?= $real_estate->ad_type($real_estate->ad_type_id) === $ad_types[$key]->name ? 'selected' : '' ?>>
@@ -32,13 +36,14 @@ partial('header'); ?>
                     </option>
                 <?php endforeach; ?>
             </select>
+            <?= isset($errors) && array_key_exists('ad_type_id', $errors) ? '<p class="mt-1 text-sm text-red-500">' . $errors['ad_type_id'] . '</p>' : '' ?>
         </div>
 
-        <div class="flex flex-col w-60 mb-4">
+        <div class="flex flex-col mb-4">
             <label for="city">Real Estate Type</label>
             <select name="re_type_id"
                     id="re_type"
-                    class="p-1 border border-black">
+                    class="p-1 mt-2 text-black">
                 <?php foreach ($re_types as $key => $value): ?>
                     <option value="<?= $re_types[$key]->id ?>"
                         <?= $real_estate->re_type($real_estate->re_type_id) === $re_types[$key]->name ? 'selected' : '' ?>>
@@ -46,60 +51,65 @@ partial('header'); ?>
                     </option>
                 <?php endforeach; ?>
             </select>
+            <?= isset($errors) && array_key_exists('re_type_id', $errors) ? '<p class="mt-1 text-sm text-red-500">' . $errors['re_type_id'] . '</p>' : '' ?>
         </div>
 
-        <div class="flex flex-col w-60 mb-4">
+        <div class="flex flex-col mb-4">
             <label for="area">Area</label>
             <input type="text"
                    name="area"
                    id="area"
-                   class="p-1 border border-black"
+                   class="p-1 mt-2 text-black"
                    value="<?= $real_estate->area ?>">
+            <?= isset($errors) && array_key_exists('area', $errors) ? '<p class="mt-1 text-sm text-red-500">' . $errors['area'] . '</p>' : '' ?>
         </div>
 
-        <div class="flex flex-col w-60 mb-4">
+        <div class="flex flex-col mb-4">
             <label for=price"">Price</label>
             <input type="text"
                    name="price"
                    id="price"
-                   class="p-1 border border-black"
+                   class="p-1 mt-2 text-black"
                    value="<?= $real_estate->price ?>">
+            <?= isset($errors) && array_key_exists('price', $errors) ? '<p class="mt-1 text-sm text-red-500">' . $errors['price'] . '</p>' : '' ?>
         </div>
 
-        <div class="flex flex-col w-60 mb-4">
+        <div class="flex flex-col mb-4">
             <label for="year">Year</label>
             <input type="text"
                    name="year"
                    id="year"
-                   class="p-1 border border-black"
+                   class="p-1 mt-2 text-black"
                    value="<?= $real_estate->year ?>">
+            <?= isset($errors) && array_key_exists('year', $errors) ? '<p class="mt-1 text-sm text-red-500">' . $errors['year'] . '</p>' : '' ?>
         </div>
 
-        <div class="flex flex-col w-60 mb-4">
+        <div class="flex flex-col mb-4">
             <label for="description">Description</label>
             <textarea name="description"
                       id="description"
-                      class="p-1 border border-black"><?= $real_estate->description ?></textarea>
+                      class="p-1 mt-2 text-black"><?= $real_estate->description ?></textarea>
+            <?= isset($errors) && array_key_exists('description', $errors) ? '<p class="mt-1 text-sm text-red-500">' . $errors['description'] . '</p>' : '' ?>
         </div>
 
-        <div class="flex flex-col w-60 mb-4">
+        <div class="flex flex-col mb-4">
             <label for="photos">Add photos</label>
             <input type="file"
                    name="photos[]"
                    id="photos"
                    multiple
-                   class="p-1 border border-black">
+                   class="p-2 mt-2 border-2 border-white">
             <?= isset($errors) && array_key_exists('photos', $errors) ? '<p class="text-sm text-red-500">' . $errors['photos'] . '</p>' : '' ?>
         </div>
 
 
-        <?php foreach ($real_estate->photos($real_estate->id) as $photo): ?>
-            <img src="<?= $photo->path ?>" alt="real estate photo" style="width: 150px; height: 150px; object-fit: cover; display: inline; border: black 2px solid; background: white">
-        <?php endforeach; ?>
-
-        <div>
-            <button class="border-2 border-black p-3 mt-4">Submit</button>
+        <div class="grid grid-cols-4 gap-4">
+            <?php foreach ($real_estate->photos($real_estate->id) as $photo): ?>
+                <img src="<?= $photo->path ?>" alt="real estate photo" class="col-span-1 w-full h-24 object-scale-down border-2 bg-white p-2 border-indigo-700"">
+            <?php endforeach; ?>
         </div>
+
+        <button class="p-3 mt-12 bg-indigo-500 w-full text-white text-lg">Submit</button>
 
     </form>
 
